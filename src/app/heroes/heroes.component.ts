4d89e942250e1ad90f012da5552700e893805503
Thiago Silva <thiagoclick2000@hotmail.com>
2023-11-06 10:45:02 -0300

    Observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,7 +19,8 @@ this.getHeroes();
 }
 
 getHeroes():void{
- this.heroes = this.heroService.getHeroes();
+ this.heroService.getHeroes().subscribe(heroes =>
+    this.heroes = heroes);
 }
 onSelect(hero: Hero) : void{
 this.selectedHero = hero;
